7efcfac38eb00f28291a20c806c98e50e687b3cc
Bittu Raj <bitturaj124365@gmail.com>
2025-10-05 23:12:57 +0530

    2nd sectin of project container that is in horizontal stacked format completed.

--- src/Components/Projects/ProjectContainer2.jsx
+++ src/Components/Projects/ProjectContainer2.jsx
@@ -4,35 +4,37 @@ import styles from "./Project2/ProjStru.module.css";
 import project1 from "../../assets/project1.jpg";
 import project2 from "../../assets/project2.jpg";
 import project3 from "../../assets/project3.png";
-
+import porject4 from "../../assets/project4.jpg";
+import project5 from "../../assets/project5.png";
+import project6 from "../../assets/project6.jpeg";
 const ProjectContainer2 = () => {
   const projects = [
     {
-      image: project1,
-      title: "Farming Assistance Web",
+      image: porject4,
+      title: "Number-Guessing",
       description:
-        "A farming assistance platform built with HTML, CSS, and JavaScript that offers chatbot integration to answer agricultural queries and provide sustainable farming tips.",
-      techStack: ["HTML", "CSS", "JavaScript"],
-      githubLink: "https://github.com/BittuRaj124365/farmingAssistanceWebsite.git",
-      liveLink: "https://bitturaj124365.github.io/farmingAssistanceWebsite/",
+        "A beginner-friendly Python project that uses the random module to generate a secret number. The player must guess the correct number with guidance provided after each attempt. Perfect for practicing loops, conditionals, and user input handling in Python.",
+      techStack: ["Python"],
+      githubLink: "https://github.com/BittuRaj124365/numberGuessingGame.git",
+      //   liveLink: "https://bitturaj124365.github.io/farmingAssistanceWebsite/",
     },
     {
-      image: project2,
-      title: "Postify Blog Platform",
+      image: project5,
+      title: "Cafe-Management",
       description:
-        "A dynamic blog website built using RESTful APIs where users can create, edit, and delete posts seamlessly through a Node.js and EJS setup.",
-      techStack: ["Node.js", "Express", "EJS"],
-      githubLink: "https://github.com/BittuRaj124365/postify.git",
+        "A simple Python-based Cafe Management System that allows customers to place multiple orders from a menu, calculates the total bill automatically, and handles unavailable items gracefully. Perfect for learning dictionaries, loops, and input handling in Python.",
+      techStack: ["Python"],
+      githubLink: "https://github.com/BittuRaj124365/cafeManagementSystem.git",
       liveLink: "#",
     },
     {
-      image: project3,
-      title: "Wanderlust Travel Listing",
+      image: project6,
+      title: "Amazon Clone",
       description:
-        "A travel listing web app that allows users to create and manage listings with integrated CRUD functionality and responsive design.",
-      techStack: ["Node.js", "Express", "MongoDB", "EJS"],
-      githubLink: "https://github.com/BittuRaj124365/wanderLust-Travel-Listing-App.git",
-      liveLink: "#",
+        "A visual replica of the Amazon website, copying the layout, design, and product arrangement for practice and portfolio purposes.",
+      techStack: ["HTML5","CSS3"],
+      githubLink: "https://github.com/BittuRaj124365/amazonClone.git",
+      liveLink: "https://bitturaj124365.github.io/amazonClone/",
     },
   ];
 
